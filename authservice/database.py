dca34e2b67f4a2165549269cfa164835aee54cc2
Ivan Kukic <ivan.kukic@gmail.com>
2020-12-26 15:14:53 +0000

    Develop

--- authservice/database.py
+++ authservice/database.py
@@ -1,16 +1,16 @@
 import os
 from pathlib import Path
 
-import databases
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-# Reads the key-value pair from .env file
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+
+# Load .env
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path, verbose=True)
 
+# Get db URI from .env
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy session used in a single request
